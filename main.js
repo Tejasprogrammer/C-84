a8355b8142ecf05492a45e0f35dd0261f51ad855
Tejasprogrammer <78012113+Tejasprogrammer@users.noreply.github.com>
2021-09-16 08:14:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,120 +1,101 @@
-canvas=document.getElementById("myCanvas");
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
- ctx=canvas.getContext("2d");
+rover_width = 100;
+rover_height = 90;
 
-rover_width=100;
-rover_height=90;
+background_image = "mars.jpg";
 
-background_image="mars.jpg";
-rover_image="rover.png";
+rover_image = "rover.png";
 
-rover_x=10;
-rover_y=10;
+rover_x = 10;
+rover_y = 10;
 
-function add()
-    {
-        background_imageTag=new image;
-        background_imageTag.onload= uploadbackground;
-        background_imageTag.src="mars.jpg";
-        
-        rover_imageTag=new image;
-        rover_imageTag.onload= uploadrover;
-        rover_imageTag.src="rover.png";
+function add() {
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
 
-    }
-
-function uploadbackground()
-{
-ctx.drawimage(background_imageTag,0,0,canvas.width,canvas.height);
-
+	rover_imgTag = new Image(); //defining a variable with a new image
+	rover_imgTag.onload = uploadrover; // setting a function, onloading this variable
+	rover_imgTag.src = rover_image;   // load image
 }
 
-function uploadrover()
-{
-    ctx.drawimage(rover_imageTag,rover_x,rover_y,rover_width,rover_height);
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
-window.addEventListener("keydown",my_keydown);
+function uploadrover() {
+	ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+
+
+window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
-keyPressed=e.keyCode;
-console.log(keyPressed);
-
-if (keyPressed=='38')
-{
-    up();
-    console.log("up");
-
-}
-
-if(keyPressed=='40')
-{
-    down();
-    console.log("down");
-
-}
-
-if(keyPressed=='37')
-{
-    left();
-    console.log("left");
-
-}
-
-if(keyPressed=='39')
-{
-    right();
-    console.log("right");
-
-}
-
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
 }
 
 function up()
 {
-   if(rover_y>=0) 
-   {
-       rover_y=rover_y-10;
-       console.log("When up arrow is pressed X="+ rover_x + "Y ="+ rover_y);
-       uploadbackground();
-        uploadrover();
-
-   }
+	if(rover_y >=0)
+	{
+		rover_y = rover_y - 10;
+		console.log("When up arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		 uploadBackground();
+		 uploadrover();
+	}
 }
-
 function down()
 {
-   if(rover_y<=500) 
-   {
-       rover_y=rover_y+10;
-       console.log("When up arrow is pressed X="+ rover_x + "Y ="+ rover_y);
-       uploadbackground();
-        uploadrover();
-
-   }
+	if(rover_y <=500)
+	{
+		rover_y =rover_y+ 10;
+		console.log("When down arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
 }
-
 function left()
 {
-   if(rover_x>=0) 
-   {
-       rover_x=rover_x-10;
-       console.log("When up arrow is pressed X="+ rover_x + "Y ="+ rover_y);
-       uploadbackground();
-        uploadrover();
-
-   }
+	if(rover_x >= 0)
+	{
+		rover_x =rover_x - 10;
+		console.log("When left arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		 uploadrover();
+	}
 }
-
 function right()
 {
-   if(rover_x<=700) 
-   {
-       rover_x=rover_x+10;
-       console.log("When up arrow is pressed X="+ rover_x + "Y ="+ rover_y);
-       uploadbackground();
-        uploadrover();
-
+	if(rover_x <= 700)
+	{
+		rover_x =rover_x + 10;
+		console.log("When right arrow is pressed,  x = " + rover_x + " | y = " +rover_y);
+		uploadBackground();
+		uploadrover();
    }
 }
+	
